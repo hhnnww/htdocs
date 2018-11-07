4c56cc6654ca452616716dd29eea740b03d2e0d9
wuweihua <175990271@qq.com>
2018-11-07 19:06:17 +0800

    1

--- wp-content/themes/tulelx/header.php
+++ wp-content/themes/tulelx/header.php
@@ -1,1 +1,9 @@
 <?php wp_head();?>
+<div class="header">
+    <div class="container row">
+        <div class="logo">
+            <h1><?php echo bloginfo('name');?></h1>
+            <p><?php echo bloginfo('description');?></p>
+        </div>
+    </div>
+</div>
--- wp-content/themes/tulelx/index.php
+++ wp-content/themes/tulelx/index.php
@@ -1,3 +1,3 @@
 <?php get_header();?>
-zhuomian
+
 <?php get_footer();?>
